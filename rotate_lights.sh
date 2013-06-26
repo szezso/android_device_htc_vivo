6cbf7e6d6b4041b2de69731bda7430c53bdd86fe
szezso <szezsoo@gmail.com>
2013-06-26 09:46:30 +0300

    Small fixup

--- rotate_lights.sh
+++ rotate_lights.sh
@@ -5,15 +5,15 @@
 if [ $1 -eq 1 ] #lights in landscape
 then
    brightness=`cat /sys/class/leds/button-backlight-portrait/brightness`
-       if [ $brightness -ne 0 ] #Prevent issue when going from landscape to landscape
+       if [ $brightness -ne 0 ]
    then
        echo $brightness > /sys/class/leds/button-backlight-landscape/brightness
        echo 0 > /sys/class/leds/button-backlight-portrait/brightness
    fi
 
 else #lights in portrait
-   brightness=`cat /sys/class/leds/button-backlight-portrait/brightness`
-   if [ $brightness -ne 0 ] #Prevent issue when going from portrait to portrait
+   brightness=`cat /sys/class/leds/button-backlight-landscape/brightness`
+   if [ $brightness -ne 0 ]
    then
        echo $brightness > /sys/class/leds/button-backlight-portrait/brightness
        echo 0 > /sys/class/leds/button-backlight-landscape/brightness
