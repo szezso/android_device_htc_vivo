3c42112e463c2ba05c4b1baecf0ff5491227412c
szezso <szezso@szezso-desktop.(none)>
2013-10-07 17:37:04 +0200

    Contains my changes, reworked soundimage and other drivers

---
+++ rotate_lights.sh
@@ -0,0 +1,21 @@
+#!/system/bin/sh
+#Softkey Rotation helper script
+#By St.Matt
+
+if [ $1 -eq 1 ] #lights in landscape
+then
+   brightness=`cat /sys/class/leds/button-backlight-portrait/brightness`
+       if [ $brightness -ne 0 ]
+   then
+       echo $brightness > /sys/class/leds/button-backlight-landscape/brightness
+       echo 0 > /sys/class/leds/button-backlight-portrait/brightness
+   fi
+
+else #lights in portrait
+   brightness=`cat /sys/class/leds/button-backlight-landscape/brightness`
+   if [ $brightness -ne 0 ]
+   then
+       echo $brightness > /sys/class/leds/button-backlight-portrait/brightness
+       echo 0 > /sys/class/leds/button-backlight-landscape/brightness
+   fi
+fi
