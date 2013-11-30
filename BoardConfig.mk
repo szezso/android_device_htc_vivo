7f9f0634b5c176ad155bdc68205334cd2b43dbfa
szezso <szezso@szezso-kitchen.(none)>
2013-11-30 10:24:30 +0100

    Init from PMEM tree

---
+++ BoardConfig.mk
@@ -0,0 +1,76 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+# Optimalizations
+ARCH_ARM_HIGH_OPTIMIZATION := true
+ARCH_ARM_HIGH_OPTIMIZATION_COMPAT := true
+TARGET_USE_O3 := true
+
+# inherit from common msm7x30
+-include device/htc/msm7x30-common/BoardConfigCommon.mk
+
+# inherit from the proprietary version
+-include vendor/htc/vivo/BoardConfigVendor.mk
+
+# Bootloader & Recovery
+TARGET_BOOTLOADER_BOARD_NAME := vivo
+TARGET_RECOVERY_FSTAB = device/htc/vivo/ramdisk/fstab.vivo
+BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun0/file
+
+# Kernel
+BOARD_KERNEL_CMDLINE := no_console_suspend=1
+BOARD_KERNEL_RECOVERY_CMDLINE := $(BOARD_KERNEL_CMDLINE) msmsdcc_power_gpio=88 androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x4400000
+BOARD_KERNEL_PAGE_SIZE := 4096
+TARGET_KERNEL_CONFIG := cyanogenmod_defconfig
+TARGET_KERNEL_SOURCE := kernel/htc/vivo
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
+
+# GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := vivo
+BOARD_VENDOR_QCOM_AMSS_VERSION := 1200
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+
+# Partitons
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 585101312
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1232072704
+BOARD_BOOTIMAGE_PARTITION_SIZE := 5194304
+BOARD_FLASH_BLOCK_SIZE := 262144
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_MISC_PARTITION := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+
+# Misc
+TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
+TARGET_PREBUILT_KERNEL := device/htc/vivo/prebuilt/kernel
+TARGET_PREBUILT_RECOVERY_KERNEL := device/htc/vivo/prebuilt/kernel
