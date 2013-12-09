87e7e01707c998c3e68f41fd7ab1b543a5e26c9c
szezso <szezsoo@gmail.com>
2013-12-09 15:56:54 +0100

    Update to KOT49E/4.4.1

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivo BUILD_ID=KRT16S BUILD_FINGERPRINT=htc_europe/htc_vivo/vivo:4.4/KRT16S/87995:user/release-keys PRIVATE_BUILD_DESC="4.4 SzeZso CyanVivo 11 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivo BUILD_ID=KOT49E BUILD_FINGERPRINT=htc_europe/htc_vivo/vivo:4.4.1/KOT49E/87995:user/release-keys PRIVATE_BUILD_DESC="4.4.1 SzeZso CyanVivo 11 release-keys"
 
 PRODUCT_NAME := cm_vivo
 PRODUCT_DEVICE := vivo
