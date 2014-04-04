ffdf61b1fd2141f23729352beece68d7702603a9
szezso <szezso@szezso-kitchen.(none)>
2014-04-04 16:42:55 +0200

    Vivo: Moving stuffs around, rework Boardconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,14 +20,40 @@
 # definition file).
 #
 
-# inherit from common msm7x30 Recovery
--include device/htc/7x30-recovery/BoardConfigCommon.mk
+# Flags
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_DMX_SUPPORT
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK
+COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB
+COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
 
-TARGET_NO_BOOTLOADER := true
+# Optimalizations
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+ARCH_ARM_HIGH_OPTIMIZATION := true
+ARCH_ARM_HIGH_OPTIMIZATION_COMPAT := true
+TARGET_USE_O3 := true
+TARGET_CORTEX_CACHE_LINE_32 := true
+TARGET_USE_SPARROW_BIONIC_OPTIMIZATION := true
+TARGET_AVOID_DRAW_TEXTURE_EXTENSION := true
+TARGET_USES_16BPPSURFACE_FOR_OPAQUE := true
 
+# Board
 TARGET_BOARD_PLATFORM := msm7x30
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 
+# Bootloader & recovery
+TARGET_NO_BOOTLOADER := true
+BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun0/file
+TARGET_BOOTLOADER_BOARD_NAME := vivo
+
+# Vendor
+BOARD_VENDOR := htc
+
+# Common stuffs for proper compilation
+TARGET_SPECIFIC_HEADER_PATH := device/htc/vivo/include
+
+# Architecture
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH := arm
@@ -36,16 +62,6 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 ARCH_ARM_HAVE_VFP := true
 USE_MALLOC_ALIGNMENT := 16
 TARGET_HAVE_TSLIB := false
-
-TARGET_EXTRA_CFLAGS += $(call cc-option,-march=armv7-a -mtune=cortex-a8)
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
-TARGET_USE_SCORPION_PLD_SET := true
-TARGET_SCORPION_BIONIC_PLDOFFS := 6
-TARGET_SCORPION_BIONIC_PLDSIZE := 128
-
-TARGET_SPECIFIC_HEADER_PATH := device/htc/msm7x30-common/include
 
 
 # Merged Wifi Defines
@@ -67,112 +83,70 @@ WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/bcmdhd.ko"
 BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
 BOARD_EGL_CFG := device/htc/msm7x30-common/egl.cfg
 BOARD_USES_ADRENO_200 := true
-TARGET_GRALLOC_USES_ASHMEM := true
 TARGET_PROVIDES_LIBLIGHTS := true
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_GENLOCK := true
 USE_OPENGL_RENDERER := true
+TARGET_USES_OVERLAY := true
 
 # QCOM
-BOARD_USE_QCOM_PMEM := true
 BOARD_USES_QCOM_GPS := true
 BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QCOM_LIBRPC := true
 BOARD_USES_QCOM_LIBS := true
-TARGET_USES_PMEM := true
 
 # Legacy
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DREFRESH_RATE=60 -DQCOM_NO_DMX_SUPPORT -DQCOM_NO_SECURE_PLAYBACK -DICS_CAMERA_BLOB
-
 BOARD_NEEDS_MEMORYHEAPPMEM := true
 BOARD_USE_NEW_LIBRIL_HTC := true
 BOARD_USES_LEGACY_RIL := true
 
-# Audio
+# Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_PREBUILT_LIBAUDIO := false
-BOARD_USES_QCOM_AUDIO_VOIPMUTE := true
-BOARD_USES_QCOM_AUDIO_RESETALL := false
-BOARD_HAVE_QCOM_FM := false
-BOARD_USES_QCOM_AUDIO_LPA := false
-BOARD_USES_QCOM_AUDIO_SPEECH := false
 
 # Boot Animation
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 TARGET_BOOTANIMATION_USE_RGB565 := true
 
+# Camera
+TARGET_DISABLE_ARM_PIE := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+BOARD_HAVE_HTC_FFC := true
+BOARD_USE_REVERSE_FFC := true
+TARGET_DISABLE_ARM_PIE := true
+USE_CAMERA_STUB := true
+BOARD_USES_HTC_CAMERA := true
+
+# ION
+TARGET_USES_ION := true
+
 # Webkit
 ENABLE_WEBGL := true
 TARGET_FORCE_CPU_UPLOAD := true
 
-BOARD_USES_HTC_CAMERA := true
-COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
-BOARD_NEEDS_MEMORYHEAPPMEM := true
-
-# Experimental
-COMMON_GLOBAL_CFLAGS += -DUSES_LEGACY_GRAPHICS
-BOARD_USES_LEGACY_GRAPHICS := true
-
-USE_CAMERA_STUB := true
-
-# Compiler Optimization
-ARCH_ARM_HIGH_OPTIMIZATION := true
-
-# Optimisations used by Qualcomm
-TARGET_CORTEX_CACHE_LINE_32 := true
-TARGET_USE_SPARROW_BIONIC_OPTIMIZATION := true
-TARGET_AVOID_DRAW_TEXTURE_EXTENSION := true
-TARGET_USES_16BPPSURFACE_FOR_OPAQUE := true
-
-# inherit from common msm7x30
--include device/htc/msm7x30-common/BoardConfigCommon.mk
-
 # inherit from the proprietary version
 -include vendor/htc/vivo/BoardConfigVendor.mk
 
-TARGET_BOOTLOADER_BOARD_NAME := vivo
-
+# Kernel
 BOARD_KERNEL_CMDLINE := no_console_suspend=1
 BOARD_KERNEL_RECOVERY_CMDLINE := $(BOARD_KERNEL_CMDLINE) msmsdcc_power_gpio=88
 BOARD_KERNEL_BASE := 0x4400000
 BOARD_KERNEL_PAGE_SIZE := 4096
+TARGET_KERNEL_CONFIG := cyanogenmod_defconfig
+TARGET_KERNEL_SOURCE := kernel/htc/vivo
 
-# needed or FFC FC's for some reason.
-TARGET_DISABLE_ARM_PIE := true
-
+# GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := vivo
 BOARD_VENDOR_QCOM_AMSS_VERSION := 1200
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 
-BOARD_HAVE_HTC_FFC := true
-BOARD_USE_REVERSE_FFC := true
-
-# Workaround for Vivo's broken overlay scaling
-#BOARD_OVERLAY_MINIFICATION_LIMIT := 2
-
-# cat /proc/emmc
-#dev:        size     erasesize name
-#mmcblk0p17: 00040000 00000200 "misc"
-#mmcblk0p21: 0087f400 00000200 "recovery"
-#mmcblk0p22: 00400000 00000200 "boot"
-#mmcblk0p25: 22dffe00 00000200 "system"
-#mmcblk0p27: 12bffe00 00000200 "cache"
-#mmcblk0p26: 496ffe00 00000200 "userdata"
-#mmcblk0p28: 014bfe00 00000200 "devlog"
-#mmcblk0p29: 00040000 00000200 "pdata"
-
+# Partitons
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 585101312
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1232072704
 BOARD_BOOTIMAGE_PARTITION_SIZE := 4194304
 BOARD_FLASH_BLOCK_SIZE := 262144
-
-TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
-TARGET_PREBUILT_KERNEL := device/htc/vivo/prebuilt/kernel
-TARGET_PREBUILT_RECOVERY_KERNEL := device/htc/vivo/prebuilt/kernel
-
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
@@ -180,10 +154,10 @@ BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
 BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
-
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
-
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
-# for recovery
-BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun0/file
+# Misc
+TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
+#TARGET_PREBUILT_KERNEL := device/htc/vivo/prebuilt/kernel
+TARGET_PREBUILT_RECOVERY_KERNEL := device/htc/vivo/prebuilt/kernel
