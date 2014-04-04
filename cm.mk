dd5757777f1f24c2425473e742b139479f940104
szezso <szezso@szezso-kitchen.(none)>
2014-04-04 16:44:40 +0200

    Vivo: IMM76L

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivo BUILD_ID=IMM76Q BUILD_FINGERPRINT=htc_europe/htc_vivo/vivo:4.0.4/IMM76Q/87995:user/release-keys PRIVATE_BUILD_DESC="4.0.4 SzeZso CM9 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivo BUILD_ID=IMM76L BUILD_FINGERPRINT=htc_europe/htc_vivo/vivo:4.0.4/IMM76L/87995:user/release-keys PRIVATE_BUILD_DESC="4.0.4 SzeZso CM9 release-keys"
 
 PRODUCT_NAME := cm_vivo
 PRODUCT_DEVICE := vivo
