487a6251b981e221f87523a5f71a892d36bd1302
szezso <szezso@szezso-kitchen.(none)>
2014-04-21 22:10:47 +0200

    Vivo: Update BT defines

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -20,4 +20,12 @@
 #define BTM_DEF_LOCAL_NAME "HTC Incredible S"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
+#define A2DP_HW_SYSFS_TUNER "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq"
+#define A2DP_HW_SYSFS_TUNER_OFF "245760"
+#define A2DP_HW_SYSFS_TUNER_ON "368640"
+
+#define BLE_INCLUDED TRUE
+#define BTA_GATT_INCLUDED TRUE
+#define SMP_INCLUDED TRUE
+
 #endif
