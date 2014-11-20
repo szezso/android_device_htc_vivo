296b4943bd0383c73bc93f41d8b636169f5a97ff
Szeleczky Zsombor <szezsoo@gmail.com>
2014-11-20 20:25:12 +0100

    Vivo: LolliPop/LRX21Q

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := IncS
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivo BUILD_ID=KTU84Q BUILD_FINGERPRINT=htc_europe/htc_vivo/vivo:4.4.4/KTU84Q/87995:user/release-keys PRIVATE_BUILD_DESC="4.4.4 SzeZso VivoKat release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivo BUILD_ID=LRX21Q BUILD_FINGERPRINT=htc_europe/htc_vivo/vivo:5.0/LRX21Q/87995:user/release-keys PRIVATE_BUILD_DESC="5.0 SzeZso VivoPop release-keys"
 
 PRODUCT_NAME := cm_vivo
 PRODUCT_DEVICE := vivo
