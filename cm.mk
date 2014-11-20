4d86778e7bc1b92e52c83587adb3dbc70dacaeb1
Szeleczky Zsombor <szezsoo@gmail.com>
2014-11-20 20:24:09 +0100

    Vivo: Remove gsm.mk inherit

--- cm.mk
+++ cm.mk
@@ -5,9 +5,6 @@ PRODUCT_RELEASE_NAME := IncS
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivo BUILD_ID=KTU84Q BUILD_FINGERPRINT=htc_europe/htc_vivo/vivo:4.4.4/KTU84Q/87995:user/release-keys PRIVATE_BUILD_DESC="4.4.4 SzeZso VivoKat release-keys"
 
 PRODUCT_NAME := cm_vivo
