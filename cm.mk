4645c0c0c54b2629f5c9c6113c1a2d439ebfb1f7
Szeleczky Zsombor <szezsoo@gmail.com>
2015-06-11 10:09:49 +0200

    Vivo: LMY48B/5.1.1

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := IncS
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivo BUILD_ID=LRX21Q BUILD_FINGERPRINT=htc_europe/htc_vivo/vivo:5.0/LRX21Q/87995:user/release-keys PRIVATE_BUILD_DESC="5.0 SzeZso VivoPop release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivo BUILD_ID=LMY48B BUILD_FINGERPRINT=htc_europe/htc_vivo/vivo:5.1.1/LMY48B/87995:user/release-keys PRIVATE_BUILD_DESC="5.1.1 SzeZso VivoPop release-keys"
 
 PRODUCT_NAME := cm_vivo
 PRODUCT_DEVICE := vivo
