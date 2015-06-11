fd12a50e0b5a0408e30a0654429c7330b082dbe9
Szeleczky Zsombor <szezsoo@gmail.com>
2015-06-11 10:44:32 +0200

    Vivo: Disable dex2oat watchdog

--- device.mk
+++ device.mk
@@ -245,6 +245,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # http://source.android.com/devices/tech/dalvik/configure.html#with_art_small_mode
 PRODUCT_PROPERTY_OVERRIDES += \
 	dalvik.vm.dex2oat-filter=interpret-only \
+	dalvik.vm.dex2oat-flags=--no-watch-dog \
 	dalvik.vm.image-dex2oat-filter=speed
 
 # Use high-density artwork where available
