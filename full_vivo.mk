4f994ede84401b46dc2b4d965a1fcfed5b6d96d1
Szeleczky Zsombor <szezsoo@gmail.com>
2015-06-12 14:59:30 +0200

    Vivo: Add bootanimation defines

--- full_vivo.mk
+++ full_vivo.mk
@@ -24,9 +24,10 @@
 # Get the long list of APNs
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
-# Camera
-PRODUCT_PACKAGES := \
-    Camera
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+TARGET_BOOTANIMATION_HALF_RES := true
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
