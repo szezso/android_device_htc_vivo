3039c74f5a4a47b8048ae772ef7a09c62d946068
Szeleczky Zsombor <szezsoo@gmail.com>
2015-06-22 10:04:20 +0200

    Vivo: Update WiFi defines

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,18 +56,15 @@ BOARD_HARDWARE_CLASS := device/htc/vivo/cmhw
 -include device/qcom/sepolicy/sepolicy.mk
 
 # Wifi
-WIFI_BAND := 802_11_ABGN
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WLAN_DEVICE := bcmdhd
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_WLAN_DEVICE := bcmdhd
 WIFI_DRIVER_FW_PATH_STA := "/system/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_AP := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
-WIFI_DRIVER_MODULE_NAME := bcmdhd
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/bcmdhd.ko"
 
 # Display
 BOARD_EGL_CFG := device/htc/vivo/prebuilt/egl.cfg
