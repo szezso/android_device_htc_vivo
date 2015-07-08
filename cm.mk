88d3234796d3537ee2576ff79a4cf60af3371f6f
Szeleczky Zsombor <szezsoo@gmail.com>
2015-07-08 19:59:37 +0200

    Vivo: Use static bootanimation (ODP)

--- cm.mk
+++ cm.mk
@@ -5,7 +5,14 @@ PRODUCT_RELEASE_NAME := IncS
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivo BUILD_ID=LMY48B BUILD_FINGERPRINT=htc_europe/htc_vivo/vivo:5.1.1/LMY48B/87995:user/release-keys PRIVATE_BUILD_DESC="5.1.1 SzeZso VivoPop release-keys"
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRODUCT_NAME=htc_vivo \
+	BUILD_FINGERPRINT="htc_wwe/htc_vivo/vivo:$(PLATFORM_VERSION)/$(BUILD_ID)/$(shell date +%Y%m%d%H%M%S):$(TARGET_BUILD_VARIANT)/test-keys" \
+	PRIVATE_BUILD_DESC="htc_vivo-$(TARGET_BUILD_VARIANT) $(PLATFORM_VERSION) $(BUILD_ID) $(shell date +%Y%m%d%H%M%S) test-keys"
 
 PRODUCT_NAME := cm_vivo
+PRODUCT_GMS_CLIENTID_BASE := android-htc-rev
 PRODUCT_DEVICE := vivo
+
+PRODUCT_BOOTANIMATION := device/htc/vivo/prebuilt/bootanimation.zip
