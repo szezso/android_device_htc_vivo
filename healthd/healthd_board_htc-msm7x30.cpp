4a534a8da42f44b6918fcbdb6dfbaef4c40e24d4
kylon <kylonux@gmail.com>
2015-11-08 20:26:56 +0100

    Vivo: Update healtd (MardonHH)

--- healthd/healthd_board_htc-msm7x30.cpp
+++ healthd/healthd_board_htc-msm7x30.cpp
@@ -23,7 +23,7 @@ void healthd_board_init(struct healthd_config *config)
     config->batteryPresentPath        = "/sys/class/power_supply/battery/present";
     config->batteryCapacityPath       = "/sys/class/power_supply/battery/capacity";
     config->batteryVoltagePath        = "/sys/class/power_supply/battery/batt_vol";
-    config->batteryTemperaturePath    = "/sys/class/power_supply/battery/batt_temp";
+    config->batteryTemperaturePath    = "/sys/class/power_supply/battery/temp";
     config->batteryTechnologyPath     = "/sys/class/power_supply/battery/technology";
 }
 
@@ -31,4 +31,24 @@ int healthd_board_battery_update(struct android::BatteryProperties *props)
 {
     // return 0 to log periodic polled battery status to kernel log
     return 1;
+}
+
+void healthd_board_mode_charger_draw_battery(struct android::BatteryProperties*)
+{
+
+}
+
+void healthd_board_mode_charger_battery_update(struct android::BatteryProperties*)
+{
+
+}
+
+void healthd_board_mode_charger_set_backlight(bool)
+{
+
+}
+
+void healthd_board_mode_charger_init()
+{
+
 }
